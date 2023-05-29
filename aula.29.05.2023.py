c23efb9c9f832a53b2fa4fc58d6c53e1e1336911
Keisy <133771120+vanderlan3390@users.noreply.github.com>
2023-05-29 09:20:42 -0300

    Update aula.29.05.2023.py

--- aula.29.05.2023.py
+++ aula.29.05.2023.py
@@ -1,1 +1,9 @@
-print ("hello word!")
+#Faça um jogo de Impar ou Par com o usuário (pergunte qual opção ele deseja, e peça um número de 1 a 5).
+
+num1 = float(input("digite o numero desejavel:"))
+
+if (result > 0 ):
+    print ("seu resultado é {} ".format(result))
+
+else :
+    print ("esse número é inválido!!!")
